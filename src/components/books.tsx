0344dade4e84f820dfc2de4b2696152cd89730eb
AdrianLafo <adrianlafontaine@gmail.com>
2023-02-04 19:56:06 -0600

    tailwindcss and typescript added

---
+++ src/components/books.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBooksApi } from "../redux/books/books";
+import Book from "./book";
+
+const Books = () => {
+  const books = useSelector((state: any) => state.booksReducer);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooksApi() as any);
+  }, []);
+
+  return (
+    <div className="bg">
+      <h2>Catálogo de libros</h2>
+      {books.map((book: any) => (
+        <Book key={book.item_id} book={book} />
+      ))}
+      <div className="Line-2" />
+    </div>
+  );
+};
+
+export default Books;
---
+++ src/types/books.ts
@@ -0,0 +1,14 @@
+export interface IBookInstance {
+  item_id: number;
+  title: string;
+  authors: string[];
+  categories: string[];
+  date: string;
+  publisher: string;
+  country: string;
+  description: string;
+  isbn10: string;
+  isbn13: string;
+  data_source: string;
+  ddc: string;
+}
