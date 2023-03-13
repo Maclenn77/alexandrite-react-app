8e0fad1b7b6ae0e135578df6c0d4ce05da2027c5
AdrianLafo <adrianlafontaine@gmail.com>
2023-03-12 22:18:05 -0600

    WIP, missing some general configurations

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container!)
+
+root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
